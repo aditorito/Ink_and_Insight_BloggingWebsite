774c7ca4a651b115fbb7214b9639cc1c5d08fb47
Aditya <as0742004@gmail.com>
2025-01-07 23:47:23 +0530

    blog routes setup

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,3 +1,22 @@
 import { Hono } from "hono";
 
-export const blogRouter = new Hono<{}>;
+export const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>;
+
+blogRouter.use("/*", async (c, next) => {
+    next()
+});
+
+blogRouter.post('/', async (c) => { });
+
+blogRouter.put('/', async (c) => { });
+
+blogRouter.get('/',  async (c)=>{});
+
+blogRouter.get('/bulk',  async (c)=>{});
+
+
