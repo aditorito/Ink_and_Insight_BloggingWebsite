52695a1f7f7b4969e1a0867ea9d535a7cda5f56b
Aditya <as0742004@gmail.com>
2025-01-08 00:14:36 +0530

    blog creating route is completed

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -8,10 +10,36 @@ export const blogRouter = new Hono<{
 }>;
 
 blogRouter.use("/*", async (c, next) => {
-    next()
+    await next();
 });
 
-blogRouter.post('/', async (c) => { });
+blogRouter.post('/', async (c) => { 
+    console.log("Reached the blog POST route");
+    const prisma = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL,
+        }).$extends(withAccelerate())
+    
+        const body = await c.req.json();        
+        
+        try {
+            const blog = await prisma.blog.create({
+                data:{
+                    authorId:body.authorId,
+                    content:body.content,
+                    title:body.title
+                }
+            })
+
+            return c.json({
+                id:blog.id
+            })
+            
+        } catch (error) {
+            c.status(411);
+            return c.text("Somthing went wrong")
+            
+        }
+});
 
 blogRouter.put('/', async (c) => { });
 
