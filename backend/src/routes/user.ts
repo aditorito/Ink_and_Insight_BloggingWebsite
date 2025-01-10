215ce4368f4be297fcf9ef040a7c24c7c0d4e0f1
Aditya <as0742004@gmail.com>
2025-01-10 13:17:36 +0530

    added zod validation

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {  sign, verify } from "hono/jwt";
+import { signupInput } from "@aditorito/medium-clone";
 
 
 export const userRouter = new Hono<{
@@ -15,8 +16,17 @@ userRouter.post("/signup", async (c)=>{
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-
     const body = await c.req.json();
+
+    const { success } = signupInput.safeParse(body);
+
+    if ( !success ) {
+        c.status(411);
+        return c.json({
+            message: "Invalid Input"
+        })
+        
+    }
     
     try {
         const user = await prisma.user.create({
