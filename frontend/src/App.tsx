ac803b47781f7faacdd524f5788d5431af9b012e
Aditya <as0742004@gmail.com>
2025-01-14 23:04:33 +0530

    added blogs pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Auth } from './pages/Signup'
 import { Blog } from './pages/Blog'
+import { Blogs } from './pages/Blogs'
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
             <Route path='/signup' element={<Auth type={'signup'}/>}/>
             <Route path='/signin'  element={<Auth type={'signin'}/>}/>
             <Route path='/blog/:id' element={<Blog/>}/>
+            <Route path='/blogs' element={<Blogs/>}/>
+
           </Routes>
         </BrowserRouter>
       </div>
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,5 @@
+export const Blogs = () => {
+    return <div>
+        <h1>Blog</h1>
+    </div>
+}
